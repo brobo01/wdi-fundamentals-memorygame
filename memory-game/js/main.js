a7befdd7bdecd879f99cf845a7abdb9005617c20
Ben Roberts <benroberts67@hotmail.co.uk>
2020-03-05 16:37:36 +0000

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,21 +1,24 @@
 const cards=["queen", "queen", "king", "king"];
-const cardsInPlay = []
+const cardsInPlay = [];
 
-var cardOne = cards[1];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
+function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
+	console.log("You found a match!");
 	} else {
-	alert("Sorry, try again");	
+	console.log("Sorry, try again");	
 	}
-} 
+}
+
+
+function flipCard(cardId){
+console.log("User flipped "+cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+if (cardsInPlay.length === 2) {
+checkForMatch()
+}
+}
 
 
 
+flipCard(0)
+flipCard(1)
