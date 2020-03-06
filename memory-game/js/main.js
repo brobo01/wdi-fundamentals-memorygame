8f6c333f06346f7ee81e32b0922d0716a54f2475
Ben Roberts <benroberts67@hotmail.co.uk>
2020-03-06 14:36:00 +0000

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,18 +1,42 @@
-const cards=["queen", "queen", "king", "king"];
+const cards=[
+{
+	rank:'Queen',
+	suit: 'Hearts',
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank:'Queen',
+	suit: 'Diamonds',
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank:'King',
+	suit: 'Hearts',
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank:'King',
+	suit: 'Diamonds',
+	cardImage: "images/king-of-diamonds.png"
+}];
 const cardsInPlay = [];
 
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	console.log("You found a match!");
+	//alert("You found a match!");
 	} else {
-	console.log("Sorry, try again");	
+	console.log("Sorry, try again");
+	//alert("Sorry, try again");	
 	}
 }
 
 
 function flipCard(cardId){
-console.log("User flipped "+cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped "+cards[cardId].rank);
+console.log(cards[cardId].cardImage)
+console.log(cards[cardId].suit)
+cardsInPlay.push(cards[cardId].rank);
 if (cardsInPlay.length === 2) {
 checkForMatch()
 }
@@ -21,4 +45,4 @@ checkForMatch()
 
 
 flipCard(0)
-flipCard(1)
+flipCard(2)
